4f7092e8795162aaf8b751f81dadc317bcf4b064
Henrique <87825553+HCMacaneiro@users.noreply.github.com>
2024-12-17 23:43:04 -0300

    Refactor substrings function and add hash return

--- substring_finder.rb
+++ substring_finder.rb
@@ -1,5 +1,10 @@
 def substrings(word, dictionary)
-  contained_substrings = dictionary.filter { |substring| word.downcase.include?(substring.downcase) }
+  contained_substrings = {}
+  dictionary.each do |substring|
+    if word.downcase.scan(substring.downcase).count > 0
+      contained_substrings[substring] = word.downcase.scan(substring.downcase).count
+    end
+  end
   contained_substrings
 end
 
